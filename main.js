5c843ddb3b9d7a7513792ab950a3959a01983de7
a_man <a.mans.opinion@gmail.com>
2024-12-22 11:27:50 +0100

    Initial codes for my simple accordion style FAQs

---
+++ main.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id : '1',
+        question: "Where can I buy your products?",
+        answer: "You can get our products from our website, from amazon and from stores near you...walk, don't be a couch potato!"
+    },
+    {
+        id : '2',
+        question: "Can I buy from anywhere in the world?",
+        answer: "Yes of course, if you order from our website or on Amazon, you can get it even if you live on the north pole!"
+    },
+    {
+        id : '3',
+        question: "I bought something from you and now there's a problem.",
+        answer: "Take a deep breath. Contemplate the transience of all things. In your mind’s eye, envision the faces of everyone you love and everything you hold dear, and let them go. Then, send us an email at mail@amansopinion.com. And if you don't get a response after 24hrs...well fuck off!"
+    },
+    {
+        id : '4',
+        question: "Do you have anything for kids?",
+        answer: "This is a trick question right?...RIGHT???"
+    },
+    {
+        id : '5',
+        question: "I love you!",
+        answer: "That's not a question you dumbass...but we love you too :)"
+    },
+];
+    
